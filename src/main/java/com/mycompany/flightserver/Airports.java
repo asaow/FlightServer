61577c2b657e86ad0638ac648020d633efd4a5e4
Persian1 <nanali.com67@yahoo.com>
2016-04-05 14:05:48 +0200

    add Airports.java

--- src/main/java/com/mycompany/flightserver/Airports.java
+++ src/main/java/com/mycompany/flightserver/Airports.java
@@ -5,10 +5,36 @@
  */
 package com.mycompany.flightserver;
 
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author Nanali67
  */
+@Path("/airports")
 public class Airports {
-    
+
+    AirportServices airportService = new AirportServices();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Airport> getAirports() {
+        return airportService.getAirports();
+    }
+
+    @GET
+    @Path("/{airportId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Airport getAirport(@PathParam("airportId") int airportId) {
+        return airportService.getAirport(airportId);
+
+    }
+
 }
